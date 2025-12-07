4bc12e4867389da5f5fb17f18aebef96fae09541
Jacob McElroy <jacoblairm@gmail.com>
2025-12-07 11:57:50 +0000

    Fix column name for apparent temperature in database schema

--- create_db.py
+++ create_db.py
@@ -7,7 +7,7 @@ c.execute('''
         id INTEGER PRIMARY KEY,
         timestamp TEXT,
         air_temperature REAL,
-        apparent_temp REAL,
+        apparent_temperature REAL,
         rel_humidity REAL,
         wind_spd_kmh REAL,
         maximum_gust_kmh REAL
